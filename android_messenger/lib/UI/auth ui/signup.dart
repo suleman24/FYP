8b93f603bb1b00befce4776d422c993d6ad14473
Suleman Anwar <63181973+suleman24@users.noreply.github.com>
2022-11-20 10:04:51 +0500

    Updated Signup Screen UI

--- android_messenger/lib/UI/auth ui/signup.dart
+++ android_messenger/lib/UI/auth ui/signup.dart
@@ -26,8 +26,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: AppColors.authscreenbackground,
-        body: Column(
+        backgroundColor: Color(0xff0f1021),
+
+      body: Column(
           children: [
 
         Container(
@@ -54,115 +55,137 @@ class _SignUpScreenState extends State<SignUpScreen> {
         ),
 
             SizedBox(
-              height: 50,
+              height: 80,
             ),
 
+            Padding(padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+            child: Container(
 
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
-              child: Container(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Enter Email",
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
-                        ),
-                      ),
-                      TextFormField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: email,
-                        decoration: InputDecoration(
-                            fillColor: Color(0xff15162d),
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                const BorderSide(width: 0.5, color: Colors.white30)),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                const BorderSide(width: 1, color: Colors.white)),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white, width: 1.0),
+              decoration: BoxDecoration(
+                color: AppColors.signuptextbackground,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+
+
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+                    child: Container(
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Enter Email",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              ),
                             ),
-                            hintText: "i.e abc@gmail.com",
-                          hintStyle: TextStyle(color: Colors.white30)
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: email,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                  fillColor: Color(0xff0f1021),
+                                  filled: true,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                      borderSide:
+                                      const BorderSide(width: 0.5, color: Colors.white30)),
+                                  enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(15),
+                                      borderSide:
+                                      const BorderSide(width: 1, color: Colors.white)),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white, width: 1.0),
+                                  ),
+                                  hintText: "i.e abc@gmail.com",
+                                  hintStyle: TextStyle(color: Colors.white30)
+                              ),
+                            ),
+
+
+
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Enter Password",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            TextFormField(
+                              controller: password,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                fillColor: Color(0xff0f1021),
+                                filled: true,
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                    borderSide:
+                                    const BorderSide(width: 0.5, color: Colors.white30)),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(15),
+                                    borderSide:
+                                    const BorderSide(width: 1, color: Colors.white)),
+
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white, width: 1.0),
+                                ),
+                                hintText: "*******",
+                                hintStyle: TextStyle(color: Colors.white30),
+                              ),
+                            ),
+                          ],
+                        )),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(30, 50, 30, 20),
+                    child: SizedBox(
+                      height: 50,
+                      width: 130,
+                      child: ElevatedButton(
+
+                        style: ElevatedButton.styleFrom(shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0),
                         ),
-                      ),
-
-
-
-                      SizedBox(
-                        height: 50,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          "Enter Password",
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.white),
+                            backgroundColor: Colors.white
                         ),
-                      ),
-                      TextFormField(
-                        controller: password,
-                        decoration: InputDecoration(
-                          fillColor: Color(0xff15162d),
-                            filled: true,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                const BorderSide(width: 0.5, color: Colors.white30)),
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
-                                borderSide:
-                                const BorderSide(width: 1, color: Colors.white)),
 
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white, width: 1.0),
+
+                        child: Center(
+                          child: Text(
+                            'Signup',
+                            style: TextStyle(
+                                color: AppColors.authscreenbackground,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
                           ),
-                            hintText: "*******",
-                            hintStyle: TextStyle(color: Colors.white30),
                         ),
+                        onPressed: ()  {
+
+                        },
                       ),
-                    ],
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(30, 50, 30, 20),
-              child: SizedBox(
-                height: 50,
-                width: 130,
-                child: ElevatedButton(
-
-                  style: ElevatedButton.styleFrom(shape: new RoundedRectangleBorder(
-                    borderRadius: new BorderRadius.circular(30.0),
-                  ),
-                  backgroundColor: Colors.white
-                  ),
-
-
-                  child: Center(
-                    child: Text(
-                      'Signup',
-                      style: TextStyle(
-                          color: AppColors.authscreenbackground,
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700),
                     ),
                   ),
-                  onPressed: ()  {
-
-                  },
-                ),
+                ],
               ),
             ),
+            )
+
+
 
 
 
@@ -173,25 +196,26 @@ class _SignUpScreenState extends State<SignUpScreen> {
           color: Colors.transparent,
 
           child: Padding(
-            padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
-            child: Container(
-              height: 50,
-              decoration: BoxDecoration(
-                color: AppColors.pureWhiteColor,
-                border: Border.all(
-                  color: AppColors.signuptextbackground,
-                  width: 2,
-                ),
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(100),
-                  topRight: Radius.circular(100),
-                ),),
+            padding: EdgeInsets.fromLTRB(80, 0, 80, 0),
+            child: GestureDetector(
+              onTap: (){
 
-              child:GestureDetector(
-                onTap: (){
 
-                },
-                child: Center(
+              },
+              child: Container(
+                height: 50,
+                decoration: BoxDecoration(
+                  color: AppColors.pureWhiteColor,
+                  border: Border.all(
+                    color: AppColors.signuptextbackground,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(100),
+                    topRight: Radius.circular(100),
+                  ),),
+
+                child:Center(
                   child: Text(
                     "Login"
                     ,
